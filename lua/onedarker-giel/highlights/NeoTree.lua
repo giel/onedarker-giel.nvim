6be8a8d8ff92f940fe9f70134c44dfdf9d2745af
Giel <mgsch@mac.com>
2025-12-05 13:43:56 +0100

    NeoTree colors

---
+++ lua/onedarker-giel/highlights/NeoTree.lua
@@ -0,0 +1,12 @@
+local Config = require "onedarker-giel.config"
+local C = require "onedarker-giel.palette"
+
+local NeoTree = {
+  NeoTreeFileName = { fg = C.hint_blue },
+  NeoTreeGitUntracked = { fg = C.warning_orange },
+  -- NeoTreeDotfile
+  -- NeoTreeMessage
+
+}
+
+return NeoTree
